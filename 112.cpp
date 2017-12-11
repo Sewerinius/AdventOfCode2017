4bfc7a1eec1d6ac4cf0d5b6c52cf80fb0abd3b4c
Sewerinius <tomek30000@gmail.com>
2017-12-11 11:05:38 +0100

    Day 11

---
+++ 112.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream in("in11.txt");
+    int p = 0, q = 0, dist = 0;
+    string dir;
+    while (getline(in, dir, ',')) {
+        if (dir == "n") {
+            p--;
+        } else if (dir == "s") {
+            p++;
+        } else if (dir == "ne") {
+            p--;
+            q++;
+        } else if (dir == "nw") {
+            q--;
+        } else if (dir == "se") {
+            q++;
+        } else if (dir == "sw") {
+            p++;
+            q--;
+        }
+        dist = max(dist, (abs(p) + abs(q) + abs(p + q)) / 2);
+    }
+
+    cout << dist;
+}
